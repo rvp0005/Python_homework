b93afd67b2aa7ecc5d0e9839a53977e0ee2afcec
Rachel Powell <rvp0005@auburn.edu>
2018-12-13 15:36:18 -0500

    All done!

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,11 @@
-#https://www.pythonforbeginners.com/dictionary/how-to-use-dictionaries-in-python
-
 import os
 import csv
 
 election_csv = os.path.join("..", "..", "Instructions", "PyPoll", "Resources", "election_data.csv")
 
-totalvotes= 0
-#makes a list called candidates
-candidates= []
-#makes a dictionary called vote counts
-#each key will be a candidate and it'll keep track of votes per candidate as it moves through the rows
-vote_count= {}
-vote_count2={}
-percentvotes = 0
+results = {}
+total = 0
+candidates=[]
 
 with open(election_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -20,31 +13,18 @@ with open(election_csv, newline="") as csvfile:
     next(csvfile)
 
     for row in csvreader:
-        #will tally up total number of votes as it goes row by row
-        totalvotes = totalvotes + 1
+        total = total + 1
+        if row[2] not in results:
+            results[row[2]] = 1 
 
-        #If the third column (who they voted for) isn't yet in the candidates list or dictionary "vote count"
-        #then add the candidates name to the candidate list and add the candidate to the dictionary "vote_count" plus add one vote
-       # if row[2] not in candidates and row[2] not in "vote_count":
-        if row[2] not in candidates and row[2] not in "vote_count":
-            candidates.append(row[2])
-            vote_count[row[2]] = 1        
-        
-        #but if the candidate is already in the list, then simply just add one to to the vote count dictionary
-        if row[2] in candidates and row[2] not in "vote_count":
-            vote_count[row[2]] = vote_count[row[2]] + 1
-    
-    numcand = len(vote_count)
+        else:
+            results[row[2]] = results[row[2]] + 1
 
-#to calculate percent votes for each candidate
-    for canname in vote_count:
-        votes= vote_count[canname]
-        name = canname
-        percentvotes=str(round((votes/totalvotes), 2))
-        #vote_count2 = {"name": canname}, {"votes": votes}, {"percent_votes": percentvotes}
-        print(f"{name} had {votes} total votes for {percentvotes}% of the total votes")
-
-    #print(candidates)
-    #print(totalvotes)
-   
-
+    candidates=list(results.keys())
+    print(f"The candidates with votes were: {candidates}.")
+    print(f"The total number of votes cast was {total}.")
+    for keys, values in results.items():
+        percent = round(((int(values)/total)*100), 4)
+        print (f"{keys}: {values} total votes | {percent}%")
+    winner = [(value, key) for key, value in results.items()]
+    print("The winner is: ",max(winner)[1])
