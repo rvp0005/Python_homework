afb4e9d70f6d1fd74c1150f51bf2d990f09b32bc
Rachel Powell <rvp0005@auburn.edu>
2018-12-13 21:18:52 -0500

    adding output file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -20,11 +20,24 @@ with open(election_csv, newline="") as csvfile:
         else:
             results[row[2]] = results[row[2]] + 1
 
+    print(f"ELECTION RESULTS")
+    pirnt(f"----------------------")
     candidates=list(results.keys())
-    print(f"The candidates with votes were: {candidates}.")
-    print(f"The total number of votes cast was {total}.")
+    print(f"Candidates: {candidates}.")
+    print(f"Total Votes: {total}.")
+
+
     for keys, values in results.items():
         percent = round(((int(values)/total)*100), 4)
         print (f"{keys}: {values} total votes | {percent}%")
     winner = [(value, key) for key, value in results.items()]
     print("The winner is: ",max(winner)[1])
+
+
+f= open("Election_Results.txt", 'w+')
+f.write(f"ELECTION RESULTS\n")
+f.write("---------------------------\n")
+f.write(f"Candidates: {candidates}\n")
+f.write(f"Total votes: {total}\n")
+#f.write(f"{toprint_keys}\n")
+#f.write(f"The winner is: {winnername}")
